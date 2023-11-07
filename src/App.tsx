cb46f24489e766312ab8376266cd8ac6a7252df4
Nikita Petrich <nikita-petrich@outlook.com>
2023-11-07 23:10:09 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Button, TextField } from '@mui/material';
 
 function App() {
+  const [attackerArmy, setAttackerArmy] = useState(10);
+  const [distributorArmy, setDistributorArmy] = useState(7);
+  const [winner, setWinner] = useState<string>();
+
+
+  const throwDice = () => {
+    let currentAttackerArmy = attackerArmy;
+    let currentDistributorArmy = distributorArmy;
+
+    while (true) {
+      let attackerDice = currentAttackerArmy >= 3 ? 3 : currentAttackerArmy === 2 ? 2 : 1;
+      let distributorDice = currentDistributorArmy >= 2 ? 2 : 1;
+
+      const attackerScore = Array.from({ length: attackerDice }, () => 1).map((i) => {
+        return (Math.floor(Math.random() * 5) + 1) + i;
+      });
+
+      const distributorScore = Array.from({ length: distributorDice }, () => 1).map((i) => {
+        return (Math.floor(Math.random() * 5) + 1) + i;
+      });
+
+      const firstAttackerDice = Math.max(...attackerScore);
+      const secondAttackerDice = attackerScore.sort((a, b) => b - a)[1];
+
+      const firstDistributorDice = Math.max(...distributorScore);
+      const secondDistributorDice = distributorScore.sort((a, b) => b - a)[1];
+  
+      if(firstDistributorDice >= firstAttackerDice) {
+        currentAttackerArmy--;
+      } else {
+        currentDistributorArmy--;
+      }
+
+      if(currentAttackerArmy <= 0 || currentDistributorArmy <= 0) break;
+
+      if(secondDistributorDice >= secondAttackerDice) {
+        currentAttackerArmy--;
+      } else {
+        currentDistributorArmy--;
+      }
+
+      if(currentAttackerArmy <= 0 || currentDistributorArmy <= 0) break;
+    }
+    setWinner(currentAttackerArmy > currentDistributorArmy ? "Attacker" : "Distributor");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection:'column', gap: '10px'}}>
+        <h1>
+          Risk Dice
+        </h1>
+        <TextField type='number'  label="Attacker Army" variant="outlined"  defaultValue={attackerArmy} onChange={(nv)=>setAttackerArmy(Number(nv.target.value))}/>
+        <TextField type='number' label="Distributor Army" variant="outlined" defaultValue={distributorArmy} onChange={(nv)=>setDistributorArmy(Number(nv.target.value))}/> 
+        <Button variant="contained" onClick={throwDice}>Throw dice!</Button>
+        {winner ? <b>The winner is: {winner}</b> : <b>Find out the winner :)</b>}     
+      </div> 
     </div>
   );
 }
